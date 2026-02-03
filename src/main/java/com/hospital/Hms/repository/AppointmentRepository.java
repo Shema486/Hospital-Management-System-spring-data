65e78665815db2792f5f7cb950b99d3e4caa325b
Shema Alphonse <ashema330@gmail.com>
2026-02-03 17:11:50 +0200

    feat(appointment): implement AppointmentController, service, and DTOs for managing appointments

--- src/main/java/com/hospital/Hms/repository/AppointmentRepository.java
+++ src/main/java/com/hospital/Hms/repository/AppointmentRepository.java
@@ -1,9 +1,18 @@
 package com.hospital.Hms.repository;
 
 import com.hospital.Hms.entity.Appointment;
+import com.hospital.Hms.entity.AppointmentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+    Page<Appointment> findByPatient_PatientId(Long patientId, Pageable pageable);
 }
--- src/main/java/com/hospital/Hms/service/AppointmentService.java
+++ src/main/java/com/hospital/Hms/service/AppointmentService.java
@@ -1,4 +1,122 @@
 package com.hospital.Hms.service;
 
+import com.hospital.Hms.dto.request.AppointmentRequest;
+import com.hospital.Hms.dto.response.AppointmentResponse;
+import com.hospital.Hms.entity.Appointment;
+import com.hospital.Hms.entity.AppointmentStatus;
+import com.hospital.Hms.entity.Doctor;
+import com.hospital.Hms.entity.Patient;
+import com.hospital.Hms.exception.BadRequestException;
+import com.hospital.Hms.exception.NotFoundException;
+import com.hospital.Hms.repository.AppointmentRepository;
+import com.hospital.Hms.repository.DoctorRepository;
+import com.hospital.Hms.repository.PatientRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
+    private final static String APPOINTMENT_NAME_CACHE="appointments";
+
+    public AppointmentService(AppointmentRepository appointmentRepository,
+                              DoctorRepository doctorRepository,
+                              PatientRepository patientRepository) {
+        this.appointmentRepository = appointmentRepository;
+        this.doctorRepository = doctorRepository;
+        this.patientRepository = patientRepository;
+    }
+
+    @Transactional
+    @CachePut(value = APPOINTMENT_NAME_CACHE,key = "#result.appointmentId")
+    public AppointmentResponse addAppointment(AppointmentRequest request) {
+        if (request.getDoctorId() == null) {
+            throw new BadRequestException("Doctor is required");
+        }
+        if (request.getPatientId() == null) {
+            throw new BadRequestException("Patient is required");
+        }
+        if (request.getAppointmentDate() == null) {
+            throw new BadRequestException("Appointment date is required");
+        }
+
+        Doctor doctor = doctorRepository.findById(request.getDoctorId())
+                .orElseThrow(() -> new NotFoundException("Doctor not found"));
+
+        Patient patient = patientRepository.findById(request.getPatientId())
+                .orElseThrow(() -> new NotFoundException("Patient not found"));
+
+        Appointment appointment = new Appointment();
+        appointment.setAppointmentDate(request.getAppointmentDate());
+        appointment.setReason(request.getReason());
+        appointment.setStatus(AppointmentStatus.SCHEDULED);
+        appointment.setDoctor(doctor);
+        appointment.setPatient(patient);
+
+        return mapToResponse(appointmentRepository.save(appointment));
+    }
+    @Transactional(readOnly = true)
+    public List<AppointmentResponse> getAllAppointments() {
+        return appointmentRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+    @Transactional
+    @CachePut(value = APPOINTMENT_NAME_CACHE,key = "#result.appointmentId")
+    public AppointmentResponse updateStatus(Long appointmentId, AppointmentStatus status) {
+        if (status == null) {
+            throw new BadRequestException("Status must not be null");
+        }
+        Appointment appointment = appointmentRepository.findById(appointmentId)
+                .orElseThrow(() ->
+                        new NotFoundException("Appointment not found with ID: " + appointmentId)
+                );
+        appointment.setStatus(status);
+        return mapToResponse(appointment);
+    }
+
+    @Transactional
+    @CacheEvict(value = APPOINTMENT_NAME_CACHE,key="#appointmentId")
+    public void deleteAppointment(Long appointmentId) {
+        Appointment appointment = appointmentRepository.findById(appointmentId)
+                .orElseThrow(() ->
+                        new NotFoundException("Appointment not found with ID: " + appointmentId)
+                );
+
+        appointmentRepository.delete(appointment);
+    }
+
+
+
+    private AppointmentResponse mapToResponse(Appointment appointment) {
+
+        String patientName = appointment.getPatient() != null
+                ? appointment.getPatient().getFirstName() + " " +
+                appointment.getPatient().getLastName()
+                : null;
+
+        String doctorName = appointment.getDoctor() != null
+                ? appointment.getDoctor().getFirstName() + " " +
+                appointment.getDoctor().getLastName()
+                : null;
+
+        return new AppointmentResponse(
+                appointment.getAppointmentId(),
+                patientName,
+                doctorName,
+                appointment.getStatus(),
+                appointment.getAppointmentDate()
+        );
+    }
 }
