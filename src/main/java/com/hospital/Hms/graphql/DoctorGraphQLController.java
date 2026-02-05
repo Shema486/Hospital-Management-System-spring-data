e1161b30118b08f0c992113d986dbd6a69e4b226
Shema Alphonse <ashema330@gmail.com>
2026-02-05 19:28:55 +0200

    feat(graphql): add DoctorGraphQLController and GraphQL schema for doctor management

---
+++ src/main/java/com/hospital/Hms/graphql/DoctorGraphQLController.java
@@ -0,0 +1,51 @@
+package com.hospital.Hms.graphql;
+
+import com.hospital.Hms.dto.request.DoctorRequest;
+import com.hospital.Hms.dto.response.DoctorResponse;
+import com.hospital.Hms.service.DoctorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class DoctorGraphQLController {
+
+    private final DoctorService doctorService;
+
+    @QueryMapping
+    public List<DoctorResponse> doctors(
+            @Argument String search,
+            @Argument Integer page,
+            @Argument Integer size
+    ) {
+        int pageNumber = (page == null) ? 0 : page;
+        int pageSize = (size == null) ? 10 : size;
+
+        return doctorService.findAllDoctor(search, PageRequest.of(pageNumber, pageSize));
+    }
+
+    @QueryMapping
+    public DoctorResponse doctorById(@Argument Long id) {
+        return doctorService.getDoctorById(id);
+    }
+    @MutationMapping
+    public DoctorResponse addDoctor(@Argument("input") DoctorRequest input) {
+        return doctorService.save(input);
+    }
+    @MutationMapping
+    public DoctorResponse updateDoctor(@Argument Long id,
+                                       @Argument("input") DoctorRequest input) {
+        return doctorService.updateDoctor(id, input);
+    }
+    @MutationMapping
+    public String deleteDoctor(@Argument Long id) {
+        doctorService.deactivateDoctor(id);
+        return "Doctor deactivated successfully";
+    }
+}
