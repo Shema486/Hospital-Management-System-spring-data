e8d8cd3b45627c6a1c33dc0d35b179742b97caf4
Shema Alphonse <ashema330@gmail.com>
2026-02-11 11:17:47 +0200

    feat(security): add JwtAuthenticationEntryPoint and AccessDeniedHandler

---
+++ src/main/java/com/hospital/Hms/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.hospital.Hms.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        response.getWriter().write("""
+        {
+          "status": 401,
+          "error": "UNAUTHORIZED",
+          "message": "Authentication is required to access this resource"
+        }
+        """);
+    }
+
+
+}
+
