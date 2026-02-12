482c7e4ed0b9c4de1c025852d8c763efef4b7f6e
Shema Alphonse <ashema330@gmail.com>
2026-02-12 16:38:15 +0200

    docs(swagger): add bearerAuth security requirement to prescription-item endpoints

--- src/main/java/com/hospital/Hms/controller/PrescriptionItemController.java
+++ src/main/java/com/hospital/Hms/controller/PrescriptionItemController.java
@@ -5,9 +5,8 @@ import com.hospital.Hms.dto.response.PrescriptionItemResponseDTO;
 import com.hospital.Hms.service.PrescriptionItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/prescription-items")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Prescription Items", description = "APIs for managing prescription medicine items")
 public class PrescriptionItemController {
 
