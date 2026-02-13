8e3e93b3364776b743915dd07dd80d8206691474
Shema Alphonse <ashema330@gmail.com>
2026-02-13 07:20:20 +0200

    `refactor(controller): remove stray semicolon in PatientController

--- src/main/java/com/hospital/Hms/controller/PatientController.java
+++ src/main/java/com/hospital/Hms/controller/PatientController.java
@@ -18,7 +18,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
